4a07cf1dea5faa52406a5c867542780b226c1294
cyauri <cyauripipa@gmail.com>
2023-05-15 23:50:07 -0500

    Creacion de Repository (CRUD), Interface y Favorite controller - PC2

--- DPA_Shopping.API/Program.cs
+++ DPA_Shopping.API/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddDbContext<StoreDbContext>(options =>
 });
 
 builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
+builder.Services.AddTransient<IFavoriteRepository, FavoriteRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
